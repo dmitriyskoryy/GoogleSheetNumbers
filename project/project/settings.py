b94e6d7bb48623bcc63e33e389aecf8096d348e7
Dmitriy Skoryy <dmitriyskoryy@gmail.com>
2022-06-14 21:12:07 +0200

    Create googlesheets.py

--- project/project/settings.py
+++ project/project/settings.py
@@ -87,6 +87,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+
+
+        # 'NAME': 'postgres',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
